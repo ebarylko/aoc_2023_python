35093fcfacd26fb82005bfd66924231a9e4c31fc
Eitan Barylko <eitan@barylko.com>
2023-12-20 21:32:16 -0800

    Added test for sum_lines

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,9 +1,18 @@
 import re
+import operator
+from functools import reduce
 
 def first_and_last_digit(line):
     '''
-    :param line: the line to extract the first and last digit from
-    :return: the first and last digit in line
+    :param line: the line to extract the calibration value from
+    :return: the first and last digit in the line
     '''
     matches = re.findall(r'[0-9]+', line)
     return int("".join([matches[i] for i in (0, -1)]))
+
+def sum_lines(lines):
+    '''
+    @param lines: A collection of lines containing calibration values
+    @return: the sum of all the calibration values in the collection
+    '''
+    return reduce(operator.add, map(first_and_last_digit, lines))
--- day_1/test_day_1.py
+++ day_1/test_day_1.py
@@ -1,9 +1,13 @@
-from day_1 import first_and_last_digit
+from day_1 import first_and_last_digit, sum_lines
 
 input = open("input.txt", "r").readlines()
+
+example_input = ["1abc2", "a1b2c3d4e5f", "treb7uchet", "pqr3stu8vwx"]
 
 def test_first_and_last_digit():
     assert 12 == first_and_last_digit("1abc2")
     assert 15 == first_and_last_digit("a1b2c3d4e5f")
     assert 77 == first_and_last_digit("treb7uchet")
 
+def test_sum_lines():
+    assert 142 == sum_lines(example_input)
