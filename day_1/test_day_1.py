2bfaabd8337de9fd25cf8d408a8abd8432fa119d
Eitan Barylko <eitan@barylko.com>
2023-12-20 21:19:29 -0800

    Added additional test cases for first_and_last_digit

--- day_1/test_day_1.py
+++ day_1/test_day_1.py
@@ -4,4 +4,6 @@ input = open("input.txt", "r").readlines()
 
 def test_first_and_last_digit():
     assert 12 == first_and_last_digit("1abc2")
+    assert 15 == first_and_last_digit("a1b2c3d4e5f")
+    assert 77 == first_and_last_digit("treb7uchet")
 
