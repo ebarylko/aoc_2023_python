1979661a8febc5aa13619c76636a644dfacf797e
Eitan Barylko <eitan@barylko.com>
2023-12-22 11:51:00 -0800

    Added functions for parsing the input

---
+++ day_2/test_day_2.py
@@ -0,0 +1,55 @@
+import toolz.functoolz as tfz
+import toolz.itertoolz as iter
+from functools import partial
+from operator import *
+
+input = open("input.txt", "r").readlines()
+
+line = open("input.txt", "r").readline()
+example = line.split(r':')
+divided = line.split(r':')[1].split(r';')
+
+def cube_sets(games):
+    """
+    @param games: a collection of games
+    @return: the cube sets for each game
+    """
+    return tfz.thread_last(
+        games,
+        iter.second,
+        lambda cube_set: cube_set.split(r';'),
+        (map, lambda cubes: cubes.split(r',')),
+        list,
+        (map, tfz.compose(list, partial(map, methodcaller('strip')))),
+        list,
+
+
+    )
+
+def split_id_and_cube_sets(games):
+    """
+    @param games: a collection of games
+    @return: a collection of pairs of game ids and their corresponding cube sets
+    """
+    return tfz.juxt(iter.first, cube_sets)(games)
+
+def parse_input(input):
+    """
+    @param input: the file containing the games
+    @return: a collection of pairs of the form game id, cube sets
+    """
+    tfz.thread_last(
+        input,
+        open,
+        lambda file: file.readlines(),
+        lambda lines: lines.split(r':'),
+        split_id_and_cube_sets
+    )
+# def test_input():
+#     assert [line.strip() for line in divided] == "i"
+
+def test_example():
+    assert split_id_and_cube_sets(example) == "i"
+
+# def test_sum_valid_ids():
+#     assert sum_valid_ids(example, example_limits) == 8
