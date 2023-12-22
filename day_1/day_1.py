82e50911b7f447bc728c91685d14f229fa2b33ad
Eitan Barylko <eitan@barylko.com>
2023-12-21 23:56:49 -0800

    Added join_digits function

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -9,13 +9,54 @@ word_to_number = {"zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 
 def to_number(digit):
     return int(digit) if digit.isdigit() else word_to_number[digit]
 
+def first_number(coll):
+    """
+    @param coll: a collection of numbers
+    @return: the first number in the collection
+    """
+    return tfz.thread_last(
+        coll,
+        first,
+        to_number
+    )
+
+def last_number(coll):
+    """
+    @param coll: a collection of numbers
+    @return: the last number in the collection
+    """
+    return tfz.thread_last(
+        coll,
+        last,
+        to_number
+    )
+
+def first_and_last(coll):
+    """
+    @param coll: a collection of numbers
+    @return: the first and last number in the collection
+    """
+    return tfz.juxt(first_number, last_number)(coll)
+
+def join_digits(digits):
+    """
+    @param digits: the first and last digits in a line
+    @return: the calibration value for the line
+    """
+    first, last = digits
+    return first * 10 + last
+
 def first_and_last_digit(line):
     """
     @param line: the line to extract the calibration value from
     @return: the first and last digit in the line
     """
-    matches = re.findall(r'(?=(\d|zero|one|two|three|four|five|six|seven|eight|nine))', line)
-    return pipe(matches, lambda m: [to_number(m[i]) for i in (0, -1)], lambda coll: coll[0] * 10 + coll[1])
+    return tfz.thread_last(
+        line,
+        (re.findall, r'(?=(\d|zero|one|two|three|four|five|six|seven|eight|nine))'),
+        first_and_last,
+        join_digits
+    )
 
 def sum_lines(lines):
     """
@@ -26,4 +67,3 @@ def sum_lines(lines):
         lines,
         (map, first_and_last_digit),
         (reduce, operator.add))
-    # return reduce(operator.add, map(first_and_last_digit, lines))
