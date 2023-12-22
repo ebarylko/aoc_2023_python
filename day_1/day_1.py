c118f7324c5a3e87acef3f6863cb184850dee293
Eitan Barylko <eitan@barylko.com>
2023-12-21 22:44:40 -0800

    Changed the regex for finding the digits

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,14 +1,27 @@
 import re
 import operator
 from functools import reduce
+from toolz import *
+
+word_to_number = {"zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
+
+def to_number(digit):
+    return int(digit) if digit.isdigit() else word_to_number[digit]
+
+def convert_all_digits_to_numerical_digits(coll):
+    """
+    @param coll: a collection of word and numerical digits
+    @return: the original collection after converting the word digits to their numerical equivalent
+    """
+    return map(to_number, coll)
 
 def first_and_last_digit(line):
-    '''
-    :param line: the line to extract the calibration value from
-    :return: the first and last digit in the line
-    '''
-    matches = re.findall(r'[0-9]+', line)
-    return int("".join([matches[i] for i in (0, -1)]))
+    """
+    @param line: the line to extract the calibration value from
+    @return: the first and last digit in the line
+    """
+    matches = re.findall(r'(?=(\d|zero|one|two|three|four|five|six|seven|eight|nine)+)', line)
+    return pipe(matches, lambda m: [to_number(m[i]) for i in (0, -1)], lambda coll: coll[0] * 10 + coll[1])
 
 def sum_lines(lines):
     '''
--- day_1/test_day_1.py
+++ day_1/test_day_1.py
@@ -1,13 +1,18 @@
-from day_1 import first_and_last_digit, sum_lines
+from day_1 import *
 
 input = open("input.txt", "r").readlines()
 
 example_input = ["1abc2", "a1b2c3d4e5f", "treb7uchet", "pqr3stu8vwx"]
 
+
 def test_first_and_last_digit():
     assert 12 == first_and_last_digit("1abc2")
     assert 15 == first_and_last_digit("a1b2c3d4e5f")
     assert 77 == first_and_last_digit("treb7uchet")
+    assert 82 == first_and_last_digit("eightwo")
 
 def test_sum_lines():
     assert 142 == sum_lines(example_input)
+    assert 18 == sum_lines(["oneight"])
+    assert 1 == sum_lines(input)
+
