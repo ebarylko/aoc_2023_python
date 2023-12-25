6e7db195e9c5df7073c599a28fb59c97572b2268
Eitan Barylko <eitan@barylko.com>
2023-12-24 18:58:21 -0800

    Refactored is_valid_game so it uses a thread_last

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -1,123 +1,52 @@
 import toolz.functoolz as tfz
-import toolz.itertoolz as iter
+import toolz.itertoolz as itz
 import toolz.dicttoolz as tzd
-from functools import partial
-from operator import *
+import operator as op
 import parsy as p
 
 number_parser = p.regex(r'\d+').map(int)
 color_parser = p.string("red") | p.string("green") | p.string("blue")
 
-number_color_parser = p.seq(number_parser << p.whitespace, color_parser << p.string("\n").optional()).map(lambda t: [t[1], t[0]])
+number_color_parser = p.seq(number_parser << p.whitespace, color_parser << p.string("\n").optional()).map(
+    lambda t: [t[1], t[0]])
+
 set_parser = number_color_parser.sep_by(p.string(",") << p.whitespace, min=1, max=3).map(dict)
+
 game_parser = set_parser.sep_by(p.string(";") << p.whitespace, min=1).map(tfz.partial(tzd.merge_with, max))
+
 id_parser = p.string("Game ") >> number_parser
 line_parser = p.seq(id_parser << p.string(": "), game_parser)
+
+
 def input_parser(lines):
+    """
+    @param lines: all the games
+    @return: a map with key value pairs of the form game id and a collection noting the maximum number
+    of each cube color in the game
+    """
     return tfz.thread_last(
         lines,
-(map, lambda line: line_parser.parse(line)),
+        (map, lambda line: line_parser.parse(line)),
         dict
     )
-def parse_input(input):
-    """
-    @param input: all the games with the corresponding sets of cubes
-    @return: a collection of pairs of the form game id, cube sets
-    """
-    # return input_parser(input)
-    # return tfz.thread_last(
-    #     input,
-    #     list,
-    #     (map, methodcaller("split", r':')),
-    #     list,
-    #     (map, tfz.compose(list, split_id_and_cube_sets)),
-    #     list,
-    #     (map, id_and_max_number_of_all_cube_types),
-    #     list
-    # )
-
-def cube_sets(games):
-    """
-    @param games: a collection of games
-    @return: the cube sets for each game
-    """
-    return tfz.thread_last(
-        games,
-        iter.second,
-        lambda cube_set: cube_set.split(r';'),
-        (map, lambda cubes: cubes.split(r',')),
-        list,
-        (map, tfz.compose(list, partial(map, methodcaller('strip')))),
-        list
-    )
-
-def game_id(game):
-    """
-    @param game: a pair of the form game_id, cube_sets
-    @return: the numerical value of the game id
-    """
-    return tfz.thread_last(
-        game,
-        iter.first,
-        lambda id: id.split(),
-        iter.last,
-        int
-    )
-
-def split_id_and_cube_sets(games):
-    """
-    @param games: a collection of games
-    @return: a collection of pairs of game ids and their corresponding cube sets
-    """
-    return tfz.juxt(game_id, cube_sets)(games)
-
-def add_cube(coll, cube):
-    return tzd.assoc(coll, iter.second(cube), int(iter.first(cube)))
-
-def add_cube_information(coll, cube_info):
-    """
-    @param coll: a map with key value pairs of the form cube color, number of cubes
-    @param cube_info: the number of cubes and the color of the cubes
-    @return: a new map with an additional entry corresponding to cube being added
-    """
-    return tfz.thread_last(
-        cube_info.split(),
-        (add_cube, coll)
-    )
 
 
-def cube_set_to_map(cube_set):
-    """
-    @param cube_set: a collection of cube colors and their corresponding number of cubes
-    @return: a map with key value pairs of the form cube color, number of cubes
-    """
-    return tfz.reduce(add_cube_information, cube_set, {})
-
-def id_and_max_number_of_all_cube_types(game):
-    """
-    @param game: a collection with the game id and the cube sets obtained in the game
-    @return: a map with the key value pairs of game id and a collection containing the maximum number of cubes for each color type
-    """
-    return {iter.first(game): tfz.thread_last(
-        game,
-        iter.second,
-        (map, cube_set_to_map),
-        list,
-        (tzd.merge_with, max),
-    )}
-
-def all_cube_numbers():
-    return itemgetter("red", "green", "blue")
 def is_valid_game(cube_limits, cube_sets):
     """
     @param cube_limits: the maximum number of red, blue, and green cubes
-    @param game: a collection with the game id and the largest number of red, blue, and green cubes drawn
+    @param cube_sets: the largest number of red, blue, and green cubes drawn in the game
     @return: true if the number of drawn cubes for every color is less than or equal to the maximum
     number of cubes for that color
     """
-    [max_red_cubes, max_green_cubes, max_blue_cubes] = cube_limits
-    [red_cubes, green_cubes, blue_cubes] = itemgetter("red", "green", "blue")(cube_sets)
-    return red_cubes <= max_red_cubes and green_cubes <= max_green_cubes and blue_cubes <= max_blue_cubes
+    return tfz.thread_last(
+        cube_sets,
+        (op.itemgetter("red", "green", "blue")),
+        (zip, cube_limits),
+        (itz.filterfalse, lambda p: p[0] >= p[1]),
+        list,
+        op.not_
+    )
+
 
 def sum_valid_ids(games, cube_limits):
     """
@@ -125,27 +54,25 @@ def sum_valid_ids(games, cube_limits):
     @param cube_limits: the maximum number of red, blue, and green cubes that can be drawn
     @return: the sum of all game ids which have cube sets which draw less than or the same amount of cubes in cube_limits for that specific color
     """
-    return tfz.thread_last(
+    return tfz.pipe(
         games,
         input_parser,
-        (tzd.valfilter, tfz.partial(is_valid_game, cube_limits)),
-        lambda m: m.keys(),
-        list,
+        tfz.partial(tzd.valfilter, tfz.partial(is_valid_game, cube_limits)),
+        op.methodcaller("keys"),
+        # lambda m: m.keys(),
         sum
     )
 
-def game_power(game):
+
+def game_power(cube_set):
     """
-    @param game: a map with key value pairs of game id, cube sets
+    @param cube_set: a map with key value pairs of game id, cube sets
     @return: the power of the set
     """
     return tfz.pipe(
-        game,
-        methodcaller("values"),
-        list,
-        iter.first,
-        itemgetter("red", "green", "blue"),
-        tfz.partial(tfz.reduce, mul)
+        cube_set,
+        op.itemgetter("red", "green", "blue"),
+        tfz.partial(tfz.reduce, op.mul)
     )
 
 
@@ -157,5 +84,7 @@ def sum_power_of_games(games):
     return tfz.thread_last(
         games,
         input_parser,
-        (map, game_power)
+        (tzd.valmap, game_power),
+        lambda m: m.values(),
+        sum
     )
--- day_2/test_day_2.py
+++ day_2/test_day_2.py
@@ -23,54 +23,73 @@ small_example = [
 ]
 
 
-
 def test_add_cube_information():
     assert add_cube_information({}, '4 green') == {"green": 4}
+
+
 def test_cube_set_to_map():
     assert cube_set_to_map(['4 green', '3 blue', '11 red']) == {"green": 4, "blue": 3, "red": 11}
+
+
 # def test_example():
-    # assert parse_input(example_input) == "1"
-    # assert input == 1
-    # assert parse_input(input) == "i"
-    # assert split_id_and_cube_sets(example) == "i"
+# assert parse_input(example_input) == "1"
+# assert input == 1
+# assert parse_input(input) == "i"
+# assert split_id_and_cube_sets(example) == "i"
 
 def test_number_parser():
     assert number_parser.parse("4") == 4
 
+
 def test_number_color_parser():
     assert number_color_parser.parse("4 green") == ["green", 4]
+
 
 def test_set_parser():
     assert set_parser.parse("3 blue, 4 red") == {"blue": 3, "red": 4}
 
+
 def test_game_parser():
     assert game_parser.parse("3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green") == {"green": 2, "blue": 6, "red": 4}
 
+
 def test_id_parser():
     assert id_parser.parse("Game 1") == 1
+
+
 def test_line_parser():
-    assert line_parser.parse("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\n") == [1, {"green": 2, "blue": 6, "red": 4}]
+    assert line_parser.parse("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\n") == [1, {"green": 2, "blue": 6,
+                                                                                                 "red": 4}]
+
 
 def test_input_parser():
-    assert input_parser(example_input) == {1:  {"blue": 6, "red": 4, "green": 2},
-                                                  2: {"blue": 4, "green": 3, "red": 1},
-                                                  3: {"blue": 6, "green": 13, "red": 20},
-                                                  4: {"green": 3, "blue": 15, "red": 14},
-                                                  5: {"green": 3, "blue": 2, "red": 6}
-                                                  }
+    assert input_parser(example_input) == {1: {"blue": 6, "red": 4, "green": 2},
+                                           2: {"blue": 4, "green": 3, "red": 1},
+                                           3: {"blue": 6, "green": 13, "red": 20},
+                                           4: {"green": 3, "blue": 15, "red": 14},
+                                           5: {"green": 3, "blue": 2, "red": 6}
+                                           }
 
-@pytest.mark.parametrize("test_input", [ "red", "green", "blue"])
+
+@pytest.mark.parametrize("test_input", ["red", "green", "blue"])
 def test_eval(test_input):
     assert color_parser.parse(test_input) == test_input
 
+
 def test_is_valid_game():
-    assert is_valid_game([12, 13, 14], {"blue": 6, "green": 2, "red": 4}) == True
-    assert is_valid_game([12, 13, 14], {"blue": 9, "green": 11, "red": 13}) == False
+    assert is_valid_game([12, 13, 14], {"blue": 6, "green": 2, "red": 4})
+    assert not is_valid_game([12, 13, 14], {"blue": 9, "green": 11, "red": 13})
+
+
 def test_sum_valid_ids():
     assert sum_valid_ids(example_input, example_limits) == 8
     assert sum_valid_ids(input, [12, 13, 14]) == 1734
 
+
 def test_game_power():
-    assert game_power({1: {"blue": 6, "green": 2, "red": 4}}) == 48
+    assert game_power({"blue": 6, "green": 2, "red": 4}) == 48
+
+
 def test_power_of_games():
     assert sum_power_of_games(example_input) == 2286
+    assert sum_power_of_games(input) == 70387
