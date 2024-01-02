f057178689af9e2b6d7e4adb12c9520f25bf79c3
Eitan Barylko <eitan@barylko.com>
2024-01-01 18:23:38 -0800

    Added an example in the docstring for find_potential_gear_positions

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -19,154 +19,11 @@ def parse_line(line):
     )
 
 
-def is_valid_coordinate(coordinate, coordinate_range):
-    return tz.first(coordinate_range) <= coordinate <= tz.last(coordinate_range)
-
-
-def is_valid_location(x_coordinate_range, y_coordinate_range, location):
-    """
-    @param location: a pair with an x, y coordinate
-    @param x_coordinate_range: the lower and upper bounds of the x coordinates
-    @param y_coordinate_range: the lower and upper bounds of the y coordinates
-    @return: true if the location has a valid x and y coordinate
-    ex: is_valid_location([0, 1], [0, 1], [1, 1]) == true
-    is_valid_location([0, 1], [0, 1], [-1, 1]) == false
-    """
-    return is_valid_coordinate(tz.first(location), x_coordinate_range) and is_valid_coordinate(tz.second(location),
-                                                                                               y_coordinate_range)
-
-
-def possible_surrounding_symbols(coordinate_restrictions, location):
-    """
-    @param location: the x,y coordinates of the digit
-    @param coordinate_restrictions: the boundaries for the x and y coordinates
-    @return: all the possible locations for a symbol
-    adjacent to the digit
-    ex: possible_part_number_locations([0,0], [[0, 1], [0, 1]]) == {(1, 0), (0, 1), (1, 1)}
-    """
-    return tz.thread_last(
-        range(-1, 2),
-        lambda pos_shifts: it.product(pos_shifts, repeat=2),
-        (filter, any),
-        (zip, it.repeat(location)),
-        (map, lambda coll: tuple(map(op.add, *coll))),
-        (filter, tz.partial(is_valid_location, tz.first(coordinate_restrictions), tz.second(coordinate_restrictions))),
-        set,
-    )
-
-
-def possible_symbol_locations(partial_schematic):
-    """
-    @param partial_schematic: two or three line of the schematic
-    @return: a collection of all the possible locations for a symbol
-    adjacent to the numbers in the second line of the partial schematic
-    ex: possible_part_number_locations([0, 1])
-    """
-    return tz.thread_last(
-        partial_schematic,
-        tz.second,
-        (re.finditer, r"\d+"),
-        (map, tz.partial(op.methodcaller("span"))),
-        list,
-        (map, lambda coll: tz.thread_last(
-            range(*coll),
-            list)),
-        list,
-        (map, tz.partial(generate_symbol_locations_for_number, [0, len(tz.first(partial_schematic)) - 1],
-                         [0, len(partial_schematic) - 1])),
-        list,
-        (zip, line_parser.parse(tz.second(partial_schematic)))
-        # (map, generate_symbol_locations_for_number, [0, len(tz.first(partial_schematic))], [0, len(partial_schematic)]),
-        # list
-        # (map, (tz.juxt(
-        #     tz.first,
-        #     tz.compose(
-        #         tz.partial(op.neg),
-        #         tz.partial(op.sub, 1),
-        #         tz.partial(tz.second))))),
-        # list,
-        # (map, )
-
-    )
-
-
-def get_all_schematic_triplets(schematics):
-    """
-    @param schematics: a collection of lines containing symbols, periods, and numbers
-    @return: the collection partitioned into sets of three schematics
-    ex: get_all_schematic_triplets([".1", ".2", ".3", ".4"]) -> [{".1", ".2", ".3"}]
-    """
-    return tz.thread_last(
-        schematics,
-        (tz.sliding_window, 3),
-        (tz.remove, lambda coll: len(coll) < 3),
-        list
-    )
-
-
-def get_first_and_last_schematic_duplet(schematic):
-    """
-    @param schematic: a collection of lines containing symbols, periods, and numbers
-    @return: the first partition after reversing it and the last partition
-    of the schematic after partitioning it into sets of two
-    ex: get_first_and_last_schematic_duplet([".1", ".2", ".3", ".4"]) == [(".2", ".1"), (".3", ".4")]
-    """
-    return tz.thread_last(
-        schematic,
-        (tz.sliding_window, 2),
-        (tz.juxt(
-            tz.compose(
-                tz.juxt(tz.last, tz.first), tz.first),
-            tz.last))
-    )
-
-
-def coordinates_spanned_by_number(x_coordinate_range):
-    """
-    @param x_coordinate_range: the x coordinates the number spans
-    @return: a collection of the coordinates spanned by the number
-    coordinates_spanned_by_number([0, 2]) = {(0, 1), (1, 1), (2, 1)}
-    """
-    return tz.thread_first(
-        x_coordinate_range,
-        (zip, it.repeat(1)),
-        set
-    )
-
-
-def generate_symbol_locations_for_number(x_coordinate_limits, y_coordinate_limits, span_of_number):
-    """
-    @param span_of_number: a collection of the x coordinates that the number occupies
-    @param x_coordinate_limits: the lower and upper bound for the x coordinates
-    @param y_coordinate_limits: the lower and upper bound for the y coordinates
-    @return: the union of all the possible symbol locations for each x coordinate
-    """
-    return tz.thread_last(
-        span_of_number,
-        coordinates_spanned_by_number,
-        list,
-        (map, tz.partial(possible_surrounding_symbols, [x_coordinate_limits, y_coordinate_limits])),
-        list,
-        (tz.juxt(tz.first,
-                 tz.compose(
-                     list,
-                     tz.partial(
-                         tz.drop, 1)))),
-        lambda coll: tz.first(coll).union(*tz.second(coll)),
-        lambda s: s - coordinates_spanned_by_number(span_of_number)
-
-        # lambda coll: x.union(*coll),
-        # list
-        # (map, list),
-        # (map, op.methodcaller("insert", 1, 1)),
-    )
-
-
 def find_numbers_and_positions(t):
     """
     @param t: a tuple with the row index and a line of the schematic
     @return: a collection of triplets of the numbers in the line, the row index, and their span
-    ex: find_numbers_and_positions((1, "46..")) -> [(46, 1, [0, 1])]
+    ex: find_numbers_and_positions((1, "46..")) -> [(46, 1, (0, 2))]
     """
     index, line = t
     parsed = parse_line(line)
@@ -219,7 +76,8 @@ def sum_part_numbers(schematic):
 def find_potential_gear_positions(t):
     """
     @param t: a tuple containing the row number and the line of the schematic corresponding to that row
-    @return: the positions of all the potential gears in the line
+    @return: the positions of all potential gears in the line
+    ex: find_potential_gear_positions((1, "12...*..*)) -> [(1, 5), (1, 8)]
     """
     row_number, line = t
     return tz.thread_last(
@@ -227,7 +85,6 @@ def find_potential_gear_positions(t):
         (re.finditer, r"\*"),
         (map, tz.compose(tz.first, op.methodcaller("span"))),
         (zip, it.repeat(row_number)),
-        list
     )
 
 
@@ -287,15 +144,14 @@ def sum_gear_ratios(schematic):
     """
     @param schematic: a collection of lines containing symbols, periods, and digits
     @return: the sum of all the gear ratios in the schematic
+    ex: sum_gear_ratios(["....*4..", "....5..."]) -> 20
     """
     numbers_and_positions = tz.thread_last(
         schematic,
         enumerate,
         (tz.mapcat, find_numbers_and_positions),
-        list,
-        (tz.groupby, tz.second)
-    )
-    # return numbers_and_positions
+        (tz.groupby, tz.second))
+
     return tz.thread_last(
         schematic,
         enumerate,
--- day_3/test_day_3.py
+++ day_3/test_day_3.py
@@ -13,62 +13,15 @@ sample = ["467..114..",
           ".664.598.."
           ]
 
-sample_2 = ["." * 6,
-            "...389",
-            "." * 6]
 
 def test_parse_line():
     assert d3.parse_line("46.1408.") == [(46, (0, 2)), (1408, (3, 7))]
     assert not d3.parse_line("........")
 
 
-def test_digit_with_range_parser():
-    assert d3.digit_with_range_parser.parse("..20..") == (20, (2, 3))
-    assert not d3.digit_with_range_parser.parse("....")
-
-
-def test_is_valid_coordinate():
-    assert d3.is_valid_coordinate(1, [0, 2])
-    assert not d3.is_valid_coordinate(-1, [0, 2])
-
-
-def test_is_valid_location():
-    assert d3.is_valid_location([0, 0], [0, 2], [0, 2]) == 1
-    assert not d3.is_valid_location([0, -1], [0, 2], [0, 2]) == 1
-    assert not d3.is_valid_location([4, -1], [0, 2], [0, 2]) == 1
-
-
-def test_get_all_schematic_triplets():
-    assert d3.get_all_schematic_triplets([".1", ".2", ".3", ".4"]) == [(".1", ".2", ".3"), (".2", ".3", ".4")]
-
-
-def test_get_first_and_last_schematic_duplet():
-    assert d3.get_first_and_last_schematic_duplet((".1", ".2", ".3", ".4")) == ((".2", ".1"), (".3", ".4"))
-
-
 def test_find_numbers_and_positions():
     assert d3.find_numbers_and_positions((1, "43..")) == [(43, 1, (0, 2))]
 
-
-def test_possible_surrounding_symbols():
-    assert (d3.possible_surrounding_symbols([[0, 2], [0, 2]], (1, 1)) ==
-            {(1, 0), (0, 0), (0, 1), (2, 1), (2, 2), (1, 2), (2, 0), (0, 2)})
-    assert (d3.possible_surrounding_symbols([[0, 1], [0, 1]], (0, 0)) ==
-            {(1, 0), (0, 1), (1, 1)})
-
-
-def test_coordinates_spanned_by_number():
-    assert d3.coordinates_spanned_by_number([0, 1]) == {(0, 1), (1, 1)}
-
-
-def test_generate_symbol_locations_for_number():
-    assert d3.generate_symbol_locations_for_number([0, 1], [0, 5], [0, 1]) == {(0, 0), (1, 0), (2, 0), (2, 1)}
-
-
-def test_possible_symbol_locations():
-    assert d3.possible_symbol_locations(("...*..",
-                                         "46.14.")) == {46: ([0, 0], [1, 0], [2, 0], [2, 1]),
-                                                        14: ([2, 0], [2, 1], [3, 0], [4, 0], [5, 0], [5, 1])}
 
 def test_is_part_number():
     assert d3.is_part_number(["...#..", "..12.."], (12, 1, (2, 4)))
@@ -102,3 +55,4 @@ def test_find_surrounding_numbers():
 
 def test_sum_gear_ratios():
     assert d3.sum_gear_ratios(sample) == 467835
+    assert d3.sum_gear_ratios(data) == 81997870
